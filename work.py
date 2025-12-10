766f0c49fd6f921ce64e7fba444f6ba36604cf84
Haritha <harithanarayananuk@gmail.com>
2025-12-10 12:20:18 +0530

    msg

--- work.py
+++ work.py
@@ -1,3 +1,15 @@
 def add(x,y):
     return x+y
-    return x*y
+
+def sub(x,y):
+    return x-y
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
